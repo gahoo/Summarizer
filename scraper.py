96898f8d41305dddb1eb7152987f7ba3557d4de7
Gahoolee <gahoolee@gmail.com>
2024-08-29 16:56:10 +0800

    add get_url_basename; fix title key error issue; add download_file, extract_markdown_images functions;

--- scraper.py
+++ scraper.py
@@ -6,12 +6,16 @@ from markdownify import markdownify
 from os import getenv as osenv
 import requests
 import os
+import re
 import pdb
 
 load_dotenv()
 FIRECRAWL_API_KEY=osenv('FIRECRAWL_API_KEY')
 JINA_API_KEY=osenv('JINA_API_KEY', None)
 MARKER_API_URL = os.getenv('MARKER_API_URL')
+
+def get_url_basename(url):
+    return os.path.splitext(os.path.basename(url))[0][:20]
 
 def write_flie(filename, content):
     with open(filename, 'w') as f:
@@ -22,7 +26,7 @@ def firecrawl(url, **kwargs):
     app = FirecrawlApp(api_key=FIRECRAWL_API_KEY)
     params = build_firecrawl_params(**kwargs)
     response = app.scrape_url(url, params)
-    filename = response['metadata']['title'] + '.firecrawl.md'
+    filename = response['metadata'].get('title', 'Untitled') + '.firecrawl.md'
     write_flie(filename, response['markdown'])
     return filename
 
@@ -35,14 +39,14 @@ def build_firecrawl_params(**kwargs):
     else:
         return {}
     
-def jina(url, **kwargs):
+def jina(url, prefix=get_url_basename(url), **kwargs):
     headers = build_jina_params(**kwargs)
     data = {'url': url}
     print(headers, data)
 
     response = requests.post('https://r.jina.ai/', headers=headers, data=data)
     json = response.json()
-    filename = json['data']['title'] + '.jina.md'
+    filename = json['data'].get('title', 'Untitled') + '.jina.md'
     write_flie(filename, json['data']['content'])
     return filename
 
@@ -71,7 +75,7 @@ def magic_markdownify(url):
     data = extractor.extract(response.text, base_url=url)
     content = markdownify(data['html']).strip()
     markdown = '# {title}\n\n{content}'.format(title=data['title'], content=content)
-    filename = data['title'] + '.magic.md'
+    filename = data.get('title', 'Untitled') + '.magic.md'
     write_flie(filename, markdown)
     return filename
     
@@ -84,19 +88,48 @@ def readability_markdownify(url):
     write_flie(filename, markdown)
     return filename
 
-def download_pdf(url, convert_to_markdown=False):
+def download_file(url):
+    filename = os.path.basename(url)
+
+    if os.path.exists(filename):
+        local_file_size = os.path.getsize(filename)
+    else:
+        local_file_size = 0
+
     try:
         response = requests.get(url, stream=True)
         response.raise_for_status()
-    except requests.exceptions.HTTPError as err:
-        print("PDF download failed:", url)
 
-    filename = os.path.basename(url)
-    with open(filename, 'wb') as file:
-        # 将响应逐块写入文件以防止内存溢出
-        for chunk in response.iter_content(chunk_size=1024):
-            file.write(chunk)
-    print('PDF downloaded: ', filename)
+        total_size = int(response.headers.get('content-length', 0))
+
+        if local_file_size == total_size:
+            print(f"File already downloaded completely: {filename}")
+            return filename
+
+        headers = {'Range': f'bytes={local_file_size}-'}
+        response = requests.get(url, headers=headers, stream=True)
+        response.raise_for_status()
+
+        with open(filename, 'ab') as file:
+            # 将响应逐块写入文件以防止内存溢出
+            for chunk in response.iter_content(chunk_size=1024):
+                file.write(chunk)
+
+        print('File downloaded: ', filename)
+        return filename
+
+    except requests.exceptions.HTTPError as err:
+        print("File download failed:", url)
+        return None
+
+def extract_markdown_images(filename):
+    with open(filename, 'r') as file:
+        content = file.read()
+    images = re.findall(r"!\[.*?\]\((.*?)\)", content, re.M)
+    return images
+
+def download_pdf(url, convert_to_markdown=False):
+    filename = download_file(url)
     if convert_to_markdown:
         filename = pdf2markdown(filename)
     return filename
