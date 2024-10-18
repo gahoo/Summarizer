8ca4805ef62dd3514b0e07cedf00c257379f3241
Gahoolee <gahoolee@gmail.com>
2024-10-18 12:34:01 +0800

    fix set_proxy decorate arg;

--- utils.py
+++ utils.py
@@ -1,24 +1,28 @@
 import functools
 import os
 
-def set_proxy(func, proxy_env='GEMINI_PROXY'):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        proxy = os.getenv(proxy_env, '')
-        if proxy:
-            http_proxy = os.environ.pop('http_proxy', '')
-            https_proxy = os.environ.pop('https_proxy', '')
+def set_proxy(proxy_env):
+    def decorate(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            proxy = os.getenv(proxy_env, '')
+            if proxy:
+                print(proxy_env, proxy)
+                http_proxy = os.environ.pop('http_proxy', '')
+                https_proxy = os.environ.pop('https_proxy', '')
 
-            os.environ['http_proxy'] = os.getenv(proxy_env, '')
-            os.environ['https_proxy'] = os.getenv(proxy_env, '')
+                os.environ['http_proxy'] = os.getenv(proxy_env, '')
+                os.environ['https_proxy'] = os.getenv(proxy_env, '')
 
-        result = func(*args, **kwargs)
+            result = func(*args, **kwargs)
 
-        if proxy:
-            os.environ['http_proxy'] = http_proxy
-            os.environ['https_proxy'] = https_proxy
-        return result
-    return wrapper
+            if proxy:
+                os.environ['http_proxy'] = http_proxy
+                os.environ['https_proxy'] = https_proxy
+            return result
+        return wrapper
+    return decorate
+
 
 def undo_proxy(func):
     @functools.wraps(func)
